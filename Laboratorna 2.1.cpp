41755f112b21b3105d37904f38cbe21e043c3504
unknown <daria.mankevych.ik.2021>
2021-09-21 22:24:13 +0300

    first branch_21

--- Laboratorna 2.1.cpp
+++ Laboratorna 2.1.cpp
@@ -13,18 +13,31 @@ int main()
 {
     double Pi = 4 * atan(1.); // число пі
     double a; // вхідний параметр
+<<<<<<< branch_21
    // double z1; // результат обчислення 1-го виразу
+=======
+    //double z1; // результат обчислення 1-го виразу
+>>>>>>> local
     double z2; // результат обчислення 2-го виразу
 
     cout << "a = "; cin >> a;
 
+<<<<<<< branch_21
     //z1 = (sin((Pi / 2) + 3 * a)) / (1 - (sin(3 * a - Pi)));
     z2 = sqrt (((5 * Pi) / 4) + ((3 * a) / 2));
 
+=======
+   // z1 = (sin((Pi / 2) + 3 * a)) / (1 - (sin(3 * a - Pi)));
+    z2 = sqrt (((5 * Pi) / 4) + ((3 * a) / 2));
+>>>>>>> local
 
     cout << endl;
     //cout << "z1 = " << z1 << endl;
     cout << "z2 = " << z2 << endl;
+<<<<<<< branch_21
+=======
+
+>>>>>>> local
     cin.get();
     return 0;
 
